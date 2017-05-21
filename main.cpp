fc0ac4abe10b8081526cb0b6838b7bfb5aa1b54c
RegulusBit <icdct1995@gmail.com>
2017-05-21 16:31:41 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -397,7 +397,7 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"192.69.216.120", "192.69.216.120"};
+static const string mainnet_seeds[] = {"192.69.216.120", ""};
 static const string testnet_seeds[] = {""};
 static const string *seeds = mainnet_seeds;
 
